7456fff75c9f76bff21f6bb3b811d6171b9a9bcc
isaacasher97 <isaac.asher97@gmail.com>
2023-05-19 23:18:32 -0400

    Commit 2 - I added more elements. My power grows with my dedication and hard work

--- app.js
+++ app.js
@@ -20,5 +20,39 @@ $(document).ready(function() {
     
     // Step 6: add a CSS rule to align all text center inside the body
     $('body').css('text-align', 'center');
+
+    // Year 2
+
+    // Step 7: Create an h2 element with your name
+    let $h2 = $('<h2>');
+    $h2.text('Isaac');
+    $container.append($h2);
     
+    // Step 8: Create an h3 element with your house
+    let $h3 = $('<h3>');
+    $h3.text('Gryffindor');
+    $container.append($h3);
+    
+    // Step 9: Create an h4 element with your pet's name and add a class for the pet type
+    let $h4Pet = $('<h4>');
+    $h4Pet.text('Storm');
+    $h4Pet.addClass('owl'); // Replace 'owl' with your pet type
+    $container.append($h4Pet);
+    
+    // Step 10: Create an h4 element with your wand name
+    let $h4Wand = $('<h4>');
+    $h4Wand.text('Holly Wand With Unicorn Hair');
+    $container.append($h4Wand);
+    
+    // Step 11: Add the font-family attribute to elements with a class matching your pet's class
+    let petClass = 'owl'; // Replace 'owl' with your pet class
+    $('.' + petClass).css('font-family', 'fantasy');
+    
+    // Console log $h1, $h2, $h3, $h4Pet, and $h4Wand
+    console.log($h1);
+    console.log($h2);
+    console.log($h3);
+    console.log($h4Pet);
+    console.log($h4Wand);
+
   });
